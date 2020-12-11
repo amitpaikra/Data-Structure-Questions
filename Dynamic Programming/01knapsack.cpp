13e356578f247f4ca936d0289b001ce3c96d62d4
Amit paikra <62157576+amitpaikra@users.noreply.github.com>
2020-12-11 14:36:48 +0530

    0 1 Knapsack Problem
    You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.

---
+++ Dynamic Programming/01knapsack.cpp
@@ -0,0 +1,31 @@
+//Question --> https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+/*
+
+You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
+*/
+
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+  
+ int i, w; 
+    int K[n + 1][W + 1]; 
+  
+ 
+    for (i = 0; i <= n; i++)  
+    { 
+        for (w = 0; w <= W; w++)  
+        { 
+            if (i == 0 || w == 0) 
+                K[i][w] = 0; 
+            else if (wt[i - 1] <= w) 
+                K[i][w] = max(val[i - 1] 
+                          + K[i - 1][w - wt[i - 1]], 
+                          K[i - 1][w]); 
+            else
+                K[i][w] = K[i - 1][w]; 
+        } 
+    } 
+  
+    return K[n][W]; 
+}
