01dca8402c8e44666cc80c242f8ae8e7e4bb5eb2
Amit paikra <62157576+amitpaikra@users.noreply.github.com>
2021-01-04 19:49:31 +0530

    String Sub sequence

---
+++ Dynamic Programming/StringSub.cpp
@@ -0,0 +1,36 @@
+//Question-->https://practice.geeksforgeeks.org/problems/find-number-of-times-a-string-occurs-as-a-subsequence3020/1
+/*
+Given two strings S1 and S2, find the number of times the second string occurs in the first string, whether continuous or discontinuous.
+*/
+
+
+    int countWays(string a, string b) 
+{ 
+    int m = a.length(); 
+    int n = b.length(); 
+  
+    int lookup[m + 1][n + 1] = { { 0 } }; 
+  
+    for (int i = 0; i <= n; ++i) 
+        lookup[0][i] = 0; 
+  
+    for (int i = 0; i <= m; ++i) 
+        lookup[i][0] = 1; 
+  
+    for (int i = 1; i <= m; i++) 
+    { 
+        for (int j = 1; j <= n; j++) 
+        { 
+           
+            if (a[i - 1] == b[j - 1]) 
+                lookup[i][j] = lookup[i - 1][j - 1] +  
+                               lookup[i - 1][j]; 
+                  
+            else
+                
+                lookup[i][j] = lookup[i - 1][j]; 
+        } 
+    } 
+  
+    return lookup[m][n]; 
+} 
